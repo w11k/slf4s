d2fb253795b78ecff5366cc954548b752de587a5
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-08-06 11:16:18 +0200

    re #1: Some refactorings, started ScalaDoc, improved code style, fixed broken mock object usage. Still TODOs!

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -1,56 +1,70 @@
 package com.weiglewilczek.slf4s
 
-import org.slf4j.{Logger => SLF4JLogger, LoggerFactory}
+import org.slf4j.{ Logger => SLF4JLogger }
 
 /**
- * Logger is a wrapper for the SLF4J
- *
+ * Thin wrapper for SLF4J making use of by-name parameters to improve performance.
  */
 trait Logger {
+
+  /**
+   * Log an error.
+   * @msg The message to be logged
+   */
+  def error(msg: => String) {
+    require (msg != null, "msg must not be null!")
+    if (slf4jLogger.isErrorEnabled) slf4jLogger error msg
+  }
+
+  /**
+   * Log an error.
+   * @msg The message to be logged
+   * @t The Throwable to be logged
+   */
+  def error(msg: => String, t: Throwable) {
+    require (msg != null, "msg must not be null!")
+    require (t != null, "t must not be null!")
+    if (slf4jLogger.isErrorEnabled) slf4jLogger.error(msg, t)
+  }
+
+  // TODO Add ScalaDoc (also other API methods)!
+  def warn(msg: => String) {
+    require (msg != null, "msg must not be null!")
+    if (slf4jLogger.isWarnEnabled) slf4jLogger warn msg
+  }
+
+  def warn(msg: => String, t: Throwable) {
+    require (msg != null, "msg must not be null!")
+    require (t != null, "t must not be null!")
+    if (slf4jLogger.isWarnEnabled) slf4jLogger.warn(msg, t)
+  }
+
   def info(msg: => String) {
     require (msg != null, "msg must not be null!")
-    if (logger.isInfoEnabled) logger info msg
+    if (slf4jLogger.isInfoEnabled) slf4jLogger info msg
   }
 
   def info(msg: => String, t: Throwable) {
     require (msg != null, "msg must not be null!")
     require (t != null, "t must not be null!")
-    if (logger.isInfoEnabled) logger.info(msg, t)
+    if (slf4jLogger.isInfoEnabled) slf4jLogger.info(msg, t)
   }
 
-  def debug(msg: => String){
+  def debug(msg: => String) {
     require (msg != null, "msg must not be null!")
-    if (logger.isDebugEnabled) logger debug msg
+    if (slf4jLogger.isDebugEnabled) slf4jLogger debug msg
   }
 
   def debug(msg: => String, t: Throwable) {
     require (msg != null, "msg must not be null!")
     require (t != null, "t must not be null!")
-    if (logger.isDebugEnabled) logger.debug(msg, t)
+    if (slf4jLogger.isDebugEnabled) slf4jLogger.debug(msg, t)
   }
 
-  def error(msg: => String){
-    require (msg != null, "msg must not be null!")
-    if (logger.isErrorEnabled) logger error msg
-  }
+  // TODO Add trace methods!
 
-  def error(msg: => String, t:Throwable){
-    require (msg != null, "msg must not be null!")
-    require (t != null, "t must not be null!")
-    if (logger.isErrorEnabled) logger.error(msg, t)
-  }
-
-  def warn(msg: => String){
-    require (msg != null, "msg must not be null!")
-    if (logger.isWarnEnabled) logger warn msg
-  }
-
-  def warn(msg: => String, t:Throwable){
-    require (msg != null, "msg must not be null!")
-    require (t != null, "msg must not be null!")
-    if (logger.isWarnEnabled) logger.warn(msg, t)
-  }
-
-  protected val logger: SLF4JLogger
+  /**
+   * The wrapped SLF4J Logger.
+   */
+  protected val slf4jLogger: SLF4JLogger
 }
-
