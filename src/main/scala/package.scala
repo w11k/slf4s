936f02b83ad26df81e546ea38ff1c0c224a40178
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-09-01 20:00:35 +0200

    closes #7: Added license & copyright headers.

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2009-2010 WeigleWilczek and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package com.weiglewilczek
 
 import org.slf4j.{ Logger => SLF4JLogger }
