95afcc4e6d06cc4c67847028083cca37a3f9c61d
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-09-03 10:23:52 +0200

    closes #10: Removed precondition checking on log methods in order to not evaluate by-name params. Added test verifying that no evaluation happens.

--- src/main/scala/logger.scala
+++ src/main/scala/logger.scala
@@ -45,101 +45,86 @@ trait Logger {
 
   /**
    * Log a message with ERROR level.
-   * @param msg The message to be logged. Must not be null!
+   * @param msg The message to be logged
    */
   def error(msg: => String) {
-    require(msg != null, "msg must not be null!")
     if (slf4jLogger.isErrorEnabled) slf4jLogger error msg
   }
 
   /**
    * Log a message with ERROR level.
-   * @param msg The message to be logged. Must not be null!
-   * @param t The Throwable to be logged. Must not be null!
+   * @param msg The message to be logged
+   * @param t The Throwable to be logged
    */
   def error(msg: => String, t: Throwable) {
-    require(msg != null, "msg must not be null!")
-    require(t != null, "t must not be null!")
     if (slf4jLogger.isErrorEnabled) slf4jLogger.error(msg, t)
   }
 
   /**
    * Log a message with WARN level.
-   * @param msg The message to be logged. Must not be null!
+   * @param msg The message to be logged
    */
   def warn(msg: => String) {
-    require(msg != null, "msg must not be null!")
     if (slf4jLogger.isWarnEnabled) slf4jLogger warn msg
   }
 
   /**
    * Log a message with WARN level.
-   * @param msg The message to be logged. Must not be null!
-   * @param t The Throwable to be logged. Must not be null!
+   * @param msg The message to be logged
+   * @param t The Throwable to be logged
    */
   def warn(msg: => String, t: Throwable) {
-    require(msg != null, "msg must not be null!")
-    require(t != null, "t must not be null!")
     if (slf4jLogger.isWarnEnabled) slf4jLogger.warn(msg, t)
   }
 
   /**
    * Log a message with INFO level.
-   * @param msg The message to be logged. Must not be null!
+   * @param msg The message to be logged
    */
   def info(msg: => String) {
-    require(msg != null, "msg must not be null!")
     if (slf4jLogger.isInfoEnabled) slf4jLogger info msg
   }
 
   /**
    * Log a message with INFO level.
-   * @param msg The message to be logged. Must not be null!
-   * @param t The Throwable to be logged. Must not be null!
+   * @param msg The message to be logged
+   * @param t The Throwable to be logged
    */
   def info(msg: => String, t: Throwable) {
-    require(msg != null, "msg must not be null!")
-    require(t != null, "t must not be null!")
     if (slf4jLogger.isInfoEnabled) slf4jLogger.info(msg, t)
   }
 
   /**
    * Log a message with DEBUG level.
-   * @param msg The message to be logged. Must not be null!
+   * @param msg The message to be logged
    */
   def debug(msg: => String) {
-    require(msg != null, "msg must not be null!")
     if (slf4jLogger.isDebugEnabled) slf4jLogger debug msg
   }
 
   /**
    * Log a message with DEBUG level.
-   * @param msg The message to be logged. Must not be null!
-   * @param t The Throwable to be logged. Must not be null!
+   * @param msg The message to be logged
+   * @param t The Throwable to be logged
    */
   def debug(msg: => String, t: Throwable) {
-    require(msg != null, "msg must not be null!")
-    require(t != null, "t must not be null!")
     if (slf4jLogger.isDebugEnabled) slf4jLogger.debug(msg, t)
   }
 
   /**
    * Log a message with TRACE level.
-   * @param msg The message to be logged. Must not be null!
+   * @param msg The message to be logged
    */
   def trace(msg: => String) {
-    require(msg != null, "msg must not be null!")
     if (slf4jLogger.isTraceEnabled) slf4jLogger trace msg
   }
 
   /**
    * Log a message with TRACE level.
-   * @param msg The message to be logged. Must not be null!
-   * @param t The Throwable to be logged. Must not be null!
+   * @param msg The message to be logged
+   * @param t The Throwable to be logged
    */
   def trace(msg: => String, t: Throwable) {
-    require(msg != null, "msg must not be null!")
-    require(t != null, "t must not be null!")
     if (slf4jLogger.isTraceEnabled) slf4jLogger.trace(msg, t)
   }
 
