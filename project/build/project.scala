5d8942a753ff45faaea76fb90260c47a70c77e7d
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-11-10 08:17:49 +0100

    closes #18: Build for Scala 2.8.1 final.

--- project/build/project.scala
+++ project/build/project.scala
@@ -21,8 +21,8 @@ class SLF4SProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugi
   val slf4jApi = "org.slf4j" % "slf4j-api" % Slf4jVersion withSources
 
   // Test
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources
-  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources
+  val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test" withSources
   val slf4jSimple = "org.slf4j" % "slf4j-simple" % Slf4jVersion % "test"
 
   // ===================================================================================================================
