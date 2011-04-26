9a0128dd2953efb35b74280ef8b5f48306ec9b83
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-04-26 18:37:09 +0200

    Support for Scala 2.9.0.RC2, using SBT 0.7.6.RC0 and bnd4sbt 1.0.2.

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -1,9 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-
-  val aquteModuleConfig =
-    ModuleConfiguration("biz.aQute", "aQute Maven Repository" at "http://www.aqute.biz/repo")
-
-  val bnd4sbt = "com.weiglewilczek.bnd4sbt" % "bnd4sbt" % "1.0.1"
+  val bnd4sbt = "com.weiglewilczek.bnd4sbt" % "bnd4sbt" % "1.0.2"
 }
